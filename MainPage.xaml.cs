f4d0818727c02be4e347aba42b6b00e50437633e
Nikolin Andoni <97979235+N1k0l1n@users.noreply.github.com>
2023-11-28 15:43:13 +0100

    bugg on configuration

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,30 +1,74 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Diagnostics;
+using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace WebRTC_WinRTC
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+
     public sealed partial class MainPage : Page
     {
+        private Microsoft.MixedReality.WebRTC.PeerConnection peerConnection;
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await InitializePeerConnection();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Exception occurred: {ex.Message}");
+            }
+        }
+
+        private async Task InitializePeerConnection()
+        {
+            var config = new Microsoft.MixedReality.WebRTC.PeerConnectionConfiguration();
+            peerConnection = new Microsoft.MixedReality.WebRTC.PeerConnection();
+            peerConnection.Connected += PeerConnection_Connected;
+            peerConnection.IceStateChanged += PeerConnection_IceStateChanged;
+            await peerConnection.InitializeAsync(config);
+        }
+
+
+        private async void PeerConnection_Connected()
+        {
+            try
+            {
+                await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                {
+                    UpdateStatus("WebRTC Status: Connected");
+                });
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Exception in PeerConnection_Connected: {ex.Message}");
+            }
+        }
+
+
+
+        private async void PeerConnection_IceStateChanged(Microsoft.MixedReality.WebRTC.IceConnectionState newState)
+        {
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                UpdateStatus($"WebRTC Status: {newState}");
+            });
+        }
+
+
+        private void UpdateStatus(string status)
+        {
+            StatusText.Text = status;
         }
     }
 }
